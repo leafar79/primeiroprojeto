d646e1a264908d7713a967c6e5c2ff4e311cecff
Rafael Almeida <almeid@gmail.com>
2018-11-26 17:42:56 -0200

    enviando primeira vez

---
+++ src/main/java/com/leafar/api/PrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.leafar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+}
