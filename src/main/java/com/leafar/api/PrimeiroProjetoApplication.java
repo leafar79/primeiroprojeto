8485e8c06bd748b1f20180b2f3650a725266311b
Rafael Almeida <almeid@gmail.com>
2018-11-27 14:41:58 -0200

    teste properties

--- src/main/java/com/leafar/api/PrimeiroProjetoApplication.java
+++ src/main/java/com/leafar/api/PrimeiroProjetoApplication.java
@@ -1,12 +1,27 @@
 package com.leafar.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeiroProjetoApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Quantidade de linhas por pagina: " + this.qtdPorPagina);
+		};
+		
+	}
+	
 }
